252426198b478ba2ce6ceb6be64ad0d06cb4fe8e
eTcilopp <kirikeza@gmail.com>
2023-04-12 20:24:50 -0600

    added gitignore to this branch

--- main/ticker.py
+++ main/ticker.py
@@ -1,5 +1,19 @@
+import yfinance as yf
+from pyfinviz import Screener
+from datetime import datetime
+
+
 def run():
-    pass
+    page = 30
+    screener = Screener(pages=[x for x in range(1, page)])
+    list_ticker = []
+    for i in range(0, page):
+        if i == 1:
+            pass
+        else:
+            for j in range(len(screener.data_frames[i])):
+                list_ticker.append(screener.data_frames[i].Ticker[j])
+
 
 if __name__=='__name__':
     run()
