f0f151545ff92869fc3a1f9f916157da6ac9f898
eTcilopp <kirikeza@gmail.com>
2023-04-14 07:38:20 -0600

    added class-based models

---
+++ main/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+class Tickers(Base):
+    __tablename__ = 'tickers'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    ticker: Mapped[str] = mapped_column(String(10), unique=True)
+    
+    def __repr__(self) -> str:
+        return self.ticker
