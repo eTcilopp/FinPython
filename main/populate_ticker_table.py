bff510f910ca76337d0dae6ef4d735e04904de40
eTcilopp <kirikeza@gmail.com>
2023-04-17 20:30:06 -0600

    modified everything

---
+++ main/populate_ticker_table.py
@@ -0,0 +1,30 @@
+from models import Database, Tickers
+from ticker import get_tickers
+
+def run(pages, session):
+    def get_company(new_ticker):
+        for ticker in fetched_data:
+            if new_ticker == ticker[0]:
+                return ticker[1]
+    
+    existing_tickers = [el[0] for el in session.query(Tickers.ticker).all()]
+    fetched_data = get_tickers(pages)   # TODO: Make as variable
+    fetched_tickers = [el[0] for el in fetched_data]
+    
+    new_tickers = list(set(fetched_tickers) - set(existing_tickers))
+    
+    new_objects = []
+    for new_ticker in new_tickers:
+        params = {
+            'ticker': new_ticker,
+            'company': get_company(new_ticker)
+        }
+        new_objects.append(Tickers(**params))
+    
+    session.add_all(new_objects)
+    session.commit()
+
+if __name__=='__main__':
+    db = Database("sqlite:///stocks_data.db")
+    session = db.session
+    run(50, session)
